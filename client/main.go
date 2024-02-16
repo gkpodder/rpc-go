60fde957cd94b52867b975244d3aac83a14adcc9
gkpodder <gkpodder2003@gmail.com>
2024-02-16 13:38:02 -0500

    added the unary and first streaming funcs

--- client/main.go
+++ client/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	pb "github.com/gkpodder/grpc-go/proto"
+
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("Localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	names := &pb.NamesList{
+		Names: []string{"John", "Doe", "Steve", "Smith"},
+	}
+
+	//callSayHello(client)
+	callSayHelloServerStream(client, names)
+}
